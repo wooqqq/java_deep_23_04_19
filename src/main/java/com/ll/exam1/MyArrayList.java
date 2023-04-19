8b994574d52bfe1d95bd12ac484157932e59fdc2
깃wooqqq <sge0225@naver.com>
2023-04-19 14:33:00 +0900

    feat : add 추가

--- src/main/java/com/ll/exam1/MyArrayList.java
+++ src/main/java/com/ll/exam1/MyArrayList.java
@@ -1,7 +1,15 @@
 package com.ll.exam1;
 
+import java.sql.Struct;
+
 public class MyArrayList<T> {
+    private int size = 0;
     public int size() {
-        return 0;
+        return size;
+    }
+
+    public boolean add(T element) {
+        size++;
+        return true;
     }
 }
--- src/test/java/com/ll/exam1/MyArrayListTest.java
+++ src/test/java/com/ll/exam1/MyArrayListTest.java
@@ -1,13 +1,26 @@
 package com.ll.exam1;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MyArrayListTest {
     @Test
-    void testSize() {
+    @DisplayName("size()")
+    void t01() {
         MyArrayList<String> list = new MyArrayList<>();
-        assertEquals(0, list.size());
+        assertThat(list.size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("add(\"사과\")")
+    void t02() {
+        MyArrayList<String> list = new MyArrayList<>();
+
+        list.add("사과");
+        list.add("포도");
+
+        assertThat(list.size()).isEqualTo(2);
     }
 }
