799381d5ddc9f95a08e40271bc4f8a8a5383a34f
깃wooqqq <sge0225@naver.com>
2023-04-19 15:25:12 +0900

    feat : 자동 증가 기능 구현

--- src/main/java/com/ll/exam1/MyArrayList.java
+++ src/main/java/com/ll/exam1/MyArrayList.java
@@ -12,10 +12,38 @@ public class MyArrayList<T> {
     }
 
     public boolean add(String element) {
+        // 만약에 공간이 부족하면 새 data 객체를 만든다.
+        makeNewDataIfNotEnough();
         data[size] = element;
 
         size++;
+
         return true;
+    }
+
+    private void makeNewDataIfNotEnough() {
+        // 먼저 공간이 부족한지 확인
+        if (ifNotEnough()) {
+            makeNewData();
+        }
+    }
+
+    private void makeNewData() {
+        // 새 배열을 만든다. (새 업체를 만든다.)
+        String[] newData = new String[data.length * 2];
+
+        // 기존 창고에 있던 물품들을 전부 새 창고로 옮긴다.
+        for (int i = 0; i < data.length; i++) {
+            newData[i] = data[i];
+        }
+
+        // 기존 창고와 계약을 해지한다.
+        // 더 이상 리스트가 기존 배열을 가리키지 않도록 하여, 자연스럽게 가비지컬렉팅이 되도록 한다.
+        data = newData;
+    }
+
+    private boolean ifNotEnough() {
+        return size >= data.length;
     }
 
     public String get(int index) {
--- src/test/java/com/ll/exam1/MyArrayListTest.java
+++ src/test/java/com/ll/exam1/MyArrayListTest.java
@@ -35,4 +35,16 @@ public class MyArrayListTest {
         assertThat(list.get(0)).isEqualTo("사과"); // data[0];
         assertThat(list.get(1)).isEqualTo("포도"); // data[1];
     }
+
+    @Test
+    @DisplayName("data(배열)의 크기가 자동으로 늘어나야 한다.")
+    void t04() {
+        MyArrayList<String> list = new MyArrayList<>();
+
+        list.add("사과");
+        list.add("포도");
+        list.add("당근");
+
+        assertThat(list.size()).isEqualTo(3);
+    }
 }
