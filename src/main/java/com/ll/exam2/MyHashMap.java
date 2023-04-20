103b2ef7f21d42f2e6941bce65412cc665687e79
깃wooqqq <sge0225@naver.com>
2023-04-20 14:30:52 +0900

    feat : entry 도입

--- src/main/java/com/ll/exam2/MyHashMap.java
+++ src/main/java/com/ll/exam2/MyHashMap.java
@@ -1,17 +1,26 @@
 package com.ll.exam2;
 
+
 public class MyHashMap<K, V> {
     private int size = 0;
-    private final Object[] keys;
-    private final Object[] values;
+    private final Entry[] entries;
+
+    private static class Entry<K, V> {
+        K key;
+        V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
 
     public MyHashMap() {
         this(2);
     }
 
     public MyHashMap(int arrayLength) {
-        keys = new Object[arrayLength];
-        values = new Object[arrayLength];
+        entries = new Entry[arrayLength];
     }
 
     public int size() {
@@ -19,8 +28,7 @@ public class MyHashMap<K, V> {
     }
 
     public V put(K key, V value) {
-        keys[size] = key;
-        values[size] = value;
+        entries[size] = new Entry<>(key, value);
 
         size++;
 
@@ -35,12 +43,12 @@ public class MyHashMap<K, V> {
         if (indexOfKey == -1) return null;
 
         // 만약에 찾으면 해당 인덱스를 가지고 values 배열에 접근해서 리턴
-        return (V) values[indexOfKey];
+        return (V) entries[indexOfKey].value;
     }
 
     private int indexOfKey(K key) {
         for (int i = 0; i < size; i++) {
-            if (key.equals(keys[i])) return i;
+            if (key.equals(entries[i].key)) return i;
         }
 
         return -1;
