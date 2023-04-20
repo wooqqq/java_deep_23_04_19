d5cf2ea330e0add2b4829cf82b3f230a70ec5ba4
깃wooqqq <sge0225@naver.com>
2023-04-20 10:22:31 +0900

    fix : add 에 T 적용

--- src/main/java/com/ll/exam1/MyArrayList.java
+++ src/main/java/com/ll/exam1/MyArrayList.java
@@ -18,7 +18,7 @@ public class MyArrayList<T> {
         return size;
     }
 
-    public boolean add(Object element) {
+    public boolean add(T element) {
         // 만약에 공간이 부족하면 새 data 객체를 만든다.
         makeNewDataIfNotEnough();
         data[size] = element;
