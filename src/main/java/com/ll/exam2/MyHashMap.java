792f220c99bfdee79e03a5ddce46c73d1cb649a7
깃wooqqq <sge0225@naver.com>
2023-04-20 13:30:23 +0900

    feat : MyHashMap 생성

---
+++ src/main/java/com/ll/exam2/MyHashMap.java
@@ -0,0 +1,7 @@
+package com.ll.exam2;
+
+public class MyHashMap<K, V> {
+    public int size() {
+        return 0;
+    }
+}
---
+++ src/test/java/com/ll/exam2/MyHashMapTest.java
@@ -0,0 +1,18 @@
+package com.ll.exam2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class MyHashMapTest {
+    @Test
+    @DisplayName("size()")
+    void t01() {
+        MyHashMap<String, Integer> map = new MyHashMap<>();
+        assertThat(map.size()).isEqualTo(0);
+    }
+}
